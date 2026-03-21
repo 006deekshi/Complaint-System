8cd926fd674c212dd59f60f38320bb3f8cc928c8
Deekshitha <limgaladeekshitha@gmail.com>
2026-03-21 23:59:28 +0530

    updated api url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 
 app.use("/api/complaint", complaintRoutes);
 app.use("/api/auth",authRoutes)
